b63ca0f3665391bc8fe91c13a2e48f421ecee4ff
fauconnier-n <fauconnier.n@gmail.com>
2022-05-19 21:03:30 +0200

    correction du return de remove_code

--- preprocessing.py
+++ preprocessing.py
@@ -1,9 +1,14 @@
 import re
 
 def remove_code(text):
-    """Retire les parties de code du post marquées par les bornes html <code>
+    """Retire les parties de code du post marquées par les bornes html <code></code>
 
     Args:
-        text (string): text
+        text (string): texte à 
+
+    Returns:
+        string: texte sans code
     """
-    re.sub('<code>(.*?)</code>', ' ', text, flags=re.MULTILINE|re.DOTALL)
+    cleaned_text = re.sub('<code>(.*?)</code>', ' ', text, flags=re.MULTILINE|re.DOTALL)
+
+    return cleaned_text
